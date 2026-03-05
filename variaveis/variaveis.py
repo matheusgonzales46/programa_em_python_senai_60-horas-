b842dccce4d06ac55885274eb286a535dfd456cd
matheusgonzales46 <matheusgaleite@gmail.com>
2026-03-05 16:37:22 -0300

    Add files via upload

---
+++ variaveis/variaveis.py
@@ -0,0 +1,47 @@
+# metodos - funções
+
+#PALAVRA() - AÇÃO - FUNÇÃO 
+
+print()
+input()
+
+palavra = 'variavel'
+
+#estrutura de dados - objeto 
+print(palavra)
+
+#--------------------------------------
+str(true)
+float(10)
+int(false)
+bool('teste')
+
+#---------------------------------------
+
+objeto = 'texto'
+
+
+
+print(len(objeto)) # tamanho 
+
+
+print(max(objeto))
+
+x = (max(objeto))
+print(x)
+print(min(objeto))
+
+text = 'isso é um texto'
+
+print(list(text))
+print(tuple(text))
+
+x = sorted(text)
+
+print(x)
+text.sort()
+
+#----------------------------------
+
+
+
